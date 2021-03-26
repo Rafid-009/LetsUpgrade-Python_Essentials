20059d134d56878301dc74ca352f4fe7b78d5110
Rafid-009 <66445118+Rafid-009@users.noreply.github.com>
2021-03-26 19:38:58 +0600

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,16 +1,20 @@
-print('Hi Captain, this ground control from YOYO Airport. You are estimately 5000 km from us.')
-alt = int(input('Please input your current altitude : '))
+import random
 
-if alt >= 6000:
-    print("You are leaving our airspace. Good Luck.")
-elif alt<=3000 :
-    print("Captain you are clear to land. Runway 5 is clear and lit for you.")
-else:
-    print("Please maintain 3000 altitude for landing clearence.\n")
-    alt = int(input('Please input your current altitude : '))
+#Assigning Runway
+n = random.randint(0,12)
+
+
+print('Hi Captain, this ground control from YOYO Airport. You are estimately 5000 km from us.\n')
+
+
+def Check(alt):
     if alt >= 6000:
         print("You are leaving our airspace. Good Luck.")
     elif alt<=3000 :
-        print("Captain, you are clear to land. Runway 5 is clear and lit for you.")
+        print("Captain you are clear to land. Runway", n, "is clear and lit for you.")
     else:
-        print("Please maintain 3000 altitude for landing clearence./n")
+        print("Please maintain 3000 altitude for landing clearence.\n")
+        Recheck()
+def Recheck():
+    Check(int(input('Please input your current altitude : ')))
+Check(int(input('Please input your current altitude : ')))
